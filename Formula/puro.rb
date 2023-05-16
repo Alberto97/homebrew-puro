0c6b04d09ae616fa80352043518af0fb8f936d51
Alberto Pedron <albertop2197@gmail.com>
2023-05-16 21:00:15 +0200

    v1.2.6

--- Formula/puro.rb
+++ Formula/puro.rb
@@ -1,8 +1,8 @@
 class Puro < Formula
     desc "Puro is a powerful tool for installing and upgrading Flutter versions, it is essential for developers that work on multiple projects or have slower internet."
     homepage "https://puro.dev/"
-    url "https://puro.dev/builds/1.2.5/darwin-x64/puro"
-    sha256 "283f5535f429b9ac31d5f7612e76e7bca36f4a0e9d152172e44e36bf4ebe27ab"
+    url "https://puro.dev/builds/1.2.6/darwin-x64/puro"
+    sha256 "26d7537527761067f55dfd9abd6b39e8e4d6f7082d8a444fc312de9b516d9d45"
     license "BSD"
 
     def install
