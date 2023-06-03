63a381e24e6f82618fecfedcabb7b14ce158ec24
Alberto Pedron <albertop2197@gmail.com>
2023-06-03 11:18:17 +0200

    Make brew audit happier

--- Formula/puro.rb
+++ Formula/puro.rb
@@ -1,15 +1,15 @@
 class Puro < Formula
-    desc "Puro is a powerful tool for installing and upgrading Flutter versions, it is essential for developers that work on multiple projects or have slower internet."
-    homepage "https://puro.dev/"
-    url "https://puro.dev/builds/1.2.6/darwin-x64/puro"
-    sha256 "26d7537527761067f55dfd9abd6b39e8e4d6f7082d8a444fc312de9b516d9d45"
-    license "BSD"
+  desc "Powerful tool for installing and upgrading Flutter versions"
+  homepage "https://puro.dev/"
+  url "https://puro.dev/builds/1.2.6/darwin-x64/puro"
+  sha256 "26d7537527761067f55dfd9abd6b39e8e4d6f7082d8a444fc312de9b516d9d45"
+  license "BSD-3-Clause"
 
-    def install
-      bin.install "puro"
-    end
-
-    test do
-      system "false"
-    end
+  def install
+    bin.install "puro"
   end
+
+  test do
+    system "false"
+  end
+end
